24f877d6565d1e53e2404a11d30d2816ff01c75c
chenxiaoping <chenxp106@163.com>
2019-11-28 10:21:48 +0800

    把数字翻译成字符串

---
+++ src/cn/gdut/jianzhi/Test45.java
@@ -0,0 +1,34 @@
+package cn.gdut.jianzhi;
+
+import java.util.Arrays;
+
+/**
+ * @Desctiption 把数组排列成最小的数
+ * 使用字符串比较、s1+s2 < s2+s2.则为s1,s2
+ * @Date 2019/11/28/028
+ **/
+public class Test45 {
+    public String PrintMinNumber(int [] numbers){
+        // 判空
+        if (numbers == null || numbers.length == 0){
+            return "";
+        }
+        // 将数字变成字符串数组
+        String [] nums = new String[numbers.length];
+        for (int i = 0;i < numbers.length; i++){
+            nums[i] = numbers[i] + "";
+        }
+        String res = "";
+        Arrays.sort(nums, (s1, s2) -> (s1+s2).compareTo(s2+s1));
+        for (String str : nums){
+            res += str;
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        Test45 test45 = new Test45();
+        int [] a = {3,32,321};
+        System.out.println(test45.PrintMinNumber(a));
+    }
+}
