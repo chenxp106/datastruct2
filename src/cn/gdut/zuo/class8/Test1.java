5d1f4e74953991f1ee3588052060f2a055c44d9e
chenxiaoping <chenxp106@163.com>
2020-02-17 22:39:14 +0800

    test2

---
+++ src/cn/gdut/zuo/class8/Test1.java
@@ -0,0 +1,12 @@
+package zuo.class8;
+
+/**
+ * @ClassName Test1
+ * @Desctiption TODO
+ * @Date 2020/2/17 22:33
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
