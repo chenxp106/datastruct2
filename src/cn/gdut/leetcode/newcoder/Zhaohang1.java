f1b64b0dc0091e11d65aa1c2cbd96cb9f05c3110
chenxp106 <chenxp106@163.com>
2020-03-27 20:17:57 +0800

    3.27号没有提交代码

---
+++ src/cn/gdut/leetcode/newcoder/Zhaohang1.java
@@ -0,0 +1,12 @@
+package cn.gdut.leetcode.newcoder;
+
+/**
+ * @ClassName Zhaohang1
+ * @Desctiption TODO
+ * @Date 2020/3/27 19:55
+ */
+public class Zhaohang1 {
+    public static void main(String[] args) {
+        System.out.println(1);
+    }
+}
