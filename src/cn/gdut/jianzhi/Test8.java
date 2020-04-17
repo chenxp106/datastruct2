cd4733c98181ab1632414907f7023b8c79d70ef5
chenxp106 <chenxp106@163.com>
2020-04-17 20:07:03 +0800

    键值-重构二叉树，中序遍历的下一个节点

--- src/cn/gdut/jianzhi/Test8.java
+++ src/cn/gdut/jianzhi/Test8.java
@@ -3,7 +3,7 @@ package cn.gdut.jianzhi;
 public class Test8 {
     public TreeLinkNode GetNext(TreeLinkNode pNode){
 
-        // 如果右子树不为空，则右子树的最左孩子即为下一个
+/*        // 如果右子树不为空，则右子树的最左孩子即为下一个
         if (pNode.right != null){
             TreeLinkNode tmp = pNode.right;
             while (tmp.left != null){
@@ -23,6 +23,27 @@ public class Test8 {
                 pNode = parent;
             }
         }
+        return null;*/
+
+        // 如果右子树不为空，则下一个节点为右子树的最左的左孩子
+        if (pNode.right != null){
+            TreeLinkNode temp = pNode.right;
+            while (temp.left != null){
+                temp = temp.left;
+            }
+            return temp;
+        }
+        // 右子树为空。往回找。找到第一个节点为左孩子为止
+        else {
+            while (pNode.next != null){
+                TreeLinkNode parent = pNode.next;
+                if (parent.left == pNode){
+                    return parent;
+                }
+                pNode = parent;
+            }
+        }
         return null;
+
     }
 }
