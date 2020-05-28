2501a09802895fb255ff1f6bbea4e84618bef5a3
chenxp106 <chenxp106@163.com>
2020-05-28 22:43:58 +0800

    leetcode-移除重复元素2，滑动窗口

--- src/cn/gdut/leetcode/arrayfind/ContainsNearbyDuplicate.java
+++ src/cn/gdut/leetcode/arrayfind/ContainsNearbyDuplicate.java
@@ -13,6 +13,19 @@ import java.util.Set;
  */
 public class ContainsNearbyDuplicate {
     public boolean containsNearbyDuplicate(int [] nums, int k){
+//        // 通过滑动窗口实现
+//        Set<Integer> set = new HashSet<>();
+//        for (int i = 0; i < nums.length; i++){
+//            if (set.contains(nums[i])){
+//                return true;
+//            }
+//            set.add(nums[i]);
+//            // 删除左边界
+//            if (set.size() > k){
+//                set.remove(nums[i-k]);
+//            }
+//        }
+//        return false;
         // 通过滑动窗口实现
         Set<Integer> set = new HashSet<>();
         for (int i = 0; i < nums.length; i++){
@@ -20,7 +33,6 @@ public class ContainsNearbyDuplicate {
                 return true;
             }
             set.add(nums[i]);
-            // 删除左边界
             if (set.size() > k){
                 set.remove(nums[i-k]);
             }
