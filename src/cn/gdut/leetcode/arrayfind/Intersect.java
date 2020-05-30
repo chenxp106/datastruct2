678ab2d12578f1c94afd46304b3f2087a734325f
chenxp106 <chenxp106@163.com>
2020-05-30 21:24:30 +0800

    leetcode-求交集

--- src/cn/gdut/leetcode/arrayfind/Intersect.java
+++ src/cn/gdut/leetcode/arrayfind/Intersect.java
@@ -11,35 +11,62 @@ import java.util.Map;
  */
 public class Intersect {
     public int [] intersect(int [] nums1, int [] nums2){
+//        Map<Integer, Integer> map1 = new HashMap<>();
+//        for (int x : nums1){
+//            map1.put(x, map1.getOrDefault(x, 0) + 1);
+//        }
+//
+//        Map<Integer, Integer> map2 = new HashMap<>();
+//        for (int x : nums2){
+//            map2.put(x, map2.getOrDefault(x, 0) + 1);
+//        }
+//
+//        int [] out = new int[nums1.length];
+//        int idx = 0;
+//        int len = 0;
+//        for (Integer key : map1.keySet()){
+//            if (map2.containsKey(key)){
+//                if (map1.get(key) > map2.get(key)){
+//                    len = map2.get(key);
+//
+//                }
+//                else {
+//                    len = map1.get(key);
+//                }
+//                int start = idx;
+//                int end = start + len;
+//                idx = end;
+//                Arrays.fill(out, start, end, key);
+//            }
+//        }
+//        return Arrays.copyOf(out, idx);
+        // 构建两个map
         Map<Integer, Integer> map1 = new HashMap<>();
-        for (int x : nums1){
+        for (int x :nums1){
             map1.put(x, map1.getOrDefault(x, 0) + 1);
         }
-
         Map<Integer, Integer> map2 = new HashMap<>();
-        for (int x : nums2){
+        for (int x :nums2){
             map2.put(x, map2.getOrDefault(x, 0) + 1);
         }
-
-        int [] out = new int[nums1.length];
-        int idx = 0;
         int len = 0;
-        for (Integer key : map1.keySet()){
-            if (map2.containsKey(key)){
-                if (map1.get(key) > map2.get(key)){
-                    len = map2.get(key);
-
+        int idx = 0;
+        int [] array = new int[nums1.length];
+        for (int x : map1.keySet()){
+            if (map2.containsKey(x)){
+                if (map1.get(x) > map2.get(x)){
+                    len = map2.get(x);
                 }
                 else {
-                    len = map1.get(key);
+                    len = map1.get(x);
                 }
                 int start = idx;
-                int end = start + len;
+                int end = idx + len;
                 idx = end;
-                Arrays.fill(out, start, end, key);
+                Arrays.fill(array, start, end, x);
             }
         }
-        return Arrays.copyOf(out, idx);
+        return Arrays.copyOf(array, idx);
     }
 
     public static void main(String[] args) {
