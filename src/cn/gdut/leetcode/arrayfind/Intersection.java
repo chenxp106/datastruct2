000b90a62bfbcaed2ade35aa31aef0562fe6e971
chenxp106 <chenxp106@163.com>
2020-05-31 21:11:41 +0800

    leetcode-求交集2

--- src/cn/gdut/leetcode/arrayfind/Intersection.java
+++ src/cn/gdut/leetcode/arrayfind/Intersection.java
@@ -11,14 +11,29 @@ import java.util.Set;
  */
 public class Intersection {
     public int [] intersection(int [] nums1, int [] nums2){
-        // 使用两个set
+//        // 使用两个set
+//        Set<Integer> set1 = new HashSet<>();
+//        for (int n : nums1){
+//            set1.add(n);
+//        }
+//        Set<Integer> set2 = new HashSet<>();
+//        for (int n : nums2){
+//            set2.add(n);
+//        }
+//        if (set1.size() < set2.size()){
+//            return find(set1, set2);
+//        }
+//        else {
+//            return find(set2, set1);
+//        }
+        // 用set
         Set<Integer> set1 = new HashSet<>();
-        for (int n : nums1){
-            set1.add(n);
+        for (int x : nums1){
+            set1.add(x);
         }
         Set<Integer> set2 = new HashSet<>();
-        for (int n : nums2){
-            set2.add(n);
+        for (int x : nums2){
+            set2.add(x);
         }
         if (set1.size() < set2.size()){
             return find(set1, set2);
@@ -28,16 +43,27 @@ public class Intersection {
         }
     }
 
+//    private int [] find(Set<Integer> set1, Set<Integer> set2){
+//        int [] output = new int[set1.size()];
+//        // 标记重复的个数
+//        int idx = 0;
+//        for (Integer x : set1){
+//            if (set2.contains(x)){
+//                output[idx++] = x;
+//            }
+//        }
+//        return Arrays.copyOf(output,idx);
+//    }
     private int [] find(Set<Integer> set1, Set<Integer> set2){
-        int [] output = new int[set1.size()];
+        int [] out = new int[set1.size()];
         // 标记重复的个数
         int idx = 0;
         for (Integer x : set1){
             if (set2.contains(x)){
-                output[idx++] = x;
+                out[idx++] = x;
             }
         }
-        return Arrays.copyOf(output,idx);
+        return Arrays.copyOf(out, idx);
     }
 }
 
