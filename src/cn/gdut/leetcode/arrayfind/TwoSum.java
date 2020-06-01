61448b27d3e2510b1ec1f547671f5c8dd881adda
chenxp106 <chenxp106@163.com>
2020-06-01 21:15:56 +0800

    leetcode-两个数的和

--- src/cn/gdut/leetcode/arrayfind/TwoSum.java
+++ src/cn/gdut/leetcode/arrayfind/TwoSum.java
@@ -23,11 +23,22 @@ public class TwoSum {
 //        }
 //        return null;
         // 一次遍历hash表
+//        Map<Integer, Integer> map = new HashMap<>();
+//        for (int i = 0; i < nums.length; i++){
+//            int complement = target - nums[i];
+//            if (map.containsKey(complement) ){
+//                return new int[] {map.get(complement),i};
+//            }
+//            map.put(nums[i], i);
+//        }
+//        throw new IllegalArgumentException("no two sum");
+
+        // 一次hash表
         Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++){
-            int complement = target - nums[i];
-            if (map.containsKey(complement) ){
-                return new int[] {map.get(complement),i};
+        for(int i = 0; i < nums.length; i++){
+            int com = target - nums[i];
+            if (map.containsKey(com)){
+                return new int[]  {map.get(com), i};
             }
             map.put(nums[i], i);
         }
